e2e548b493b779f4c91e9116573e480ce2e5ea8f
alexisTobar <tobaralexis.89@gmail.com>
2025-11-17 23:56:55 -0300

    Fix: Configura STATIC_ROOT para Whitenoise (arregla el CSS del Admin)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT es VITAL en producción. Le dice a 'collectstatic' dónde guardar los archivos.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
